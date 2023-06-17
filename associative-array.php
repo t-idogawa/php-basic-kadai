69e77aeec8fcc9b9a52b3a95573cb504ffd7b44a
t-idogawa <125930571+t-idogawa@users.noreply.github.com>
2023-06-18 08:33:07 +0900

    Add files via upload

---
+++ associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+    </head>
+    <body>
+        <p>
+            <?php
+               $food_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+               print_r($food_data);
+            ?>
+        </p>
+    </body>
+</html>
